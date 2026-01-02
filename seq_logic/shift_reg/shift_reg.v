bb920cea25dbf96bdf3503ad8ee7fcf95c962953
Terry Chen <terrychen187@gmail.com>
2026-01-02 09:47:21 -0500

    update shifting direction

--- seq_logic/shift_reg/shift_reg.v
+++ seq_logic/shift_reg/shift_reg.v
@@ -10,10 +10,19 @@ module shift_reg ( // synchronous 4-bit shift reg (D flip-flop implementation)
             q <= 4'b0;
         end
         else begin
+            // Right shift : q <= {d, q[3:1]};
+            q[0] <= q[1];
+            q[1] <= q[2];
+            q[2] <= q[3];
+            q[3] <= d;
+
+            /* Left shift : q <= {q[2:0], d};
             q[0] <= d;
             q[1] <= q[0];
             q[2] <= q[1];
             q[3] <= q[2];
+            */
+
         end
     end
 

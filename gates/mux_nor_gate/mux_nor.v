6023fdb2265c25f362d536d4a3eb0abb6ee4770a
Terry Chen <terrychen187@gmail.com>
2026-02-05 02:43:15 -0500

    add nor gate module with mux implementation

---
+++ gates/mux_nor_gate/mux_nor.v
@@ -0,0 +1,18 @@
+module nor_gate (
+    input a, b,
+    output reg out
+)
+    //  0 0 = 1
+    //  0 1 = 0
+    //  1 0 = 0
+    //  1 1 = 0
+    // ~(a | b)
+
+    // b = 1 -> mux b : 0
+    // b = 0 -> mux b : 1
+    // a = 1 -> mux a : 0
+    // a = 0 -> mux a : mux b
+
+    assign out = a ? 0 : ((b) ? 0 : 1);
+
+endmodule
